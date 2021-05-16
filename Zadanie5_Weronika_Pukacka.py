e356cc0c6ff53a16b1bc2e59ae8b8e98d400e2c2
WeronikaPu <82755413+WeronikaPu@users.noreply.github.com>
2021-05-16 15:24:25 +0200

    Add files via upload

---
+++ Zadanie5_Weronika_Pukacka.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 15:22:35 2021
+
+@author: Weronika
+"""
+
+def suma (a,b,c):
+    d=a+b+c
+    print(d)
+    
+    suma (1.6.7)
+    suma (2,4,6)
+    suma (3,5,9)
